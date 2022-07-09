d3887de43bfa9dd6d0b4e1437638014511ec6fd0
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-07-09 19:53:29 +0700

    update project with add logging interceptor

--- TourismApp/app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/network/ApiConfig.kt
+++ TourismApp/app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/network/ApiConfig.kt
@@ -1,13 +1,30 @@
 package com.dicoding.tourismapp.core.data.source.remote.network
 
+import com.dicoding.tourismapp.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiConfig {
+    private fun provideOkhttpClient() : OkHttpClient {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(
+                if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+            )
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+
     fun provideApiService() : ApiService {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://tourism-api.dicoding.dev/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(provideOkhttpClient())
             .build()
         return retrofit.create(ApiService::class.java)
     }
