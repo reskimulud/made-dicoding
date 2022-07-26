b41af2db6dabab48e01b69d26aa908998dd08016
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-07-26 10:17:26 +0700

    create project My Simple Login for Koin exercise

---
+++ MySimpleLogin/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.dicoding.mysimplelogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.mysimplelogin.databinding.ActivityMainBinding
+import org.koin.android.ext.android.inject
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    val userRepository: UserRepository by inject()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val sesi = SessionManager(this)
+//        userRepository = UserRepository.getInstance(sesi)
+
+        if (userRepository.isUserLogin()) {
+            moveToHomeActivity()
+        }
+
+        binding.btnLogin.setOnClickListener {
+            saveSession()
+        }
+    }
+
+    private fun saveSession() {
+        userRepository.loginUser(binding.edUsername.text.toString())
+        moveToHomeActivity()
+    }
+
+    private fun moveToHomeActivity() {
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
+    }
+}
