4596e7436a4074a0b9a49aa591515f0010406500
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-08-01 15:20:27 +0700

    update project using Dagger Hilt

--- TourismAppDagger/app/src/main/java/com/dicoding/tourismapp/core/di/RepositoryModule.kt
+++ TourismAppDagger/app/src/main/java/com/dicoding/tourismapp/core/di/RepositoryModule.kt
@@ -4,8 +4,11 @@ import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
 import dagger.Binds
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
-@Module(includes = [NetworkModule::class, DatabaseModule::class])
+@InstallIn(SingletonComponent::class)
+@Module
 abstract class RepositoryModule {
     @Binds
     abstract fun bindRepository(repository: TourismRepository): ITourismRepository
--- TourismAppDagger/app/src/main/java/com/dicoding/tourismapp/home/HomeFragment.kt
+++ TourismAppDagger/app/src/main/java/com/dicoding/tourismapp/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.dicoding.tourismapp.home
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,19 +8,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.dicoding.tourismapp.MyApplication
 import com.dicoding.tourismapp.R
 import com.dicoding.tourismapp.core.data.Resource
 import com.dicoding.tourismapp.core.ui.TourismAdapter
-import com.dicoding.tourismapp.core.ui.ViewModelFactory
 import com.dicoding.tourismapp.databinding.FragmentHomeBinding
 import com.dicoding.tourismapp.detail.DetailTourismActivity
-import javax.inject.Inject
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
-    @Inject lateinit var factory: ViewModelFactory
-    private val homeViewModel: HomeViewModel by viewModels { factory }
+    private val homeViewModel: HomeViewModel by viewModels()
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
@@ -32,11 +29,6 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        (requireActivity().application as MyApplication).appComponent.inject(this)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
