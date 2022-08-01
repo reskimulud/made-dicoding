388c55c6aea3c6cca18a2fd23cf4099134d9a630
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-08-01 13:49:16 +0700

    create project Tourism App for implement dagger

---
+++ TourismAppDagger/app/src/main/java/com/dicoding/tourismapp/core/di/RepositoryModule.kt
@@ -0,0 +1,12 @@
+package com.dicoding.tourismapp.core.di
+
+import com.dicoding.tourismapp.core.data.TourismRepository
+import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
+import dagger.Binds
+import dagger.Module
+
+@Module(includes = [NetworkModule::class, DatabaseModule::class])
+abstract class RepositoryModule {
+    @Binds
+    abstract fun bindRepository(repository: TourismRepository): ITourismRepository
+}
