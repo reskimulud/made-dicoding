21a0f75a58606ad60c9ed26fa9a29c456a0a292b
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-08-04 16:20:50 +0700

    update project My Simple Login to implement Split Install

--- MySimpleLoginModularization/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
+++ MySimpleLoginModularization/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
@@ -6,6 +6,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.core.UserRepository
 import com.dicoding.mysimplelogin.databinding.ActivityHomeBinding
+import com.google.android.play.core.splitinstall.SplitInstallManager
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
 
 class HomeActivity : AppCompatActivity() {
 
@@ -29,10 +32,32 @@ class HomeActivity : AppCompatActivity() {
 
         binding.fabChat.setOnClickListener {
             try {
-                moveToChatActivity()
+                installChatModule()
             } catch (err: Exception) {
                 Toast.makeText(this@HomeActivity, "Module not found", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun installChatModule() {
+        val splitInstallManager = SplitInstallManagerFactory.create(this)
+        val moduleChat = "chat"
+        if (splitInstallManager.installedModules.contains(moduleChat)) {
+            moveToChatActivity()
+            Toast.makeText(this@HomeActivity, "Module already installed. Open it", Toast.LENGTH_SHORT).show()
+        } else {
+            val request = SplitInstallRequest.newBuilder()
+                .addModule(moduleChat)
+                .build()
+
+            splitInstallManager.startInstall(request)
+                .addOnSuccessListener {
+                    Toast.makeText(this@HomeActivity, "Module installed. Open it", Toast.LENGTH_SHORT).show()
+                    moveToChatActivity()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this@HomeActivity, "Module not installed", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 
